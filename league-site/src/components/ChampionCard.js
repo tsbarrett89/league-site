fee8a29a65deff5474800f41688747b291b89a55
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-28 09:58:01 -0600

    getting all pushed for review

--- league-site/src/components/ChampionCard.js
+++ league-site/src/components/ChampionCard.js
@@ -4,13 +4,14 @@ import React from 'react';
 const ChampionCard = (props) => {
 
     return (
-        <div>
+        <div key={props.key}>
+            <p>test</p>
             <h3>{props.name}</h3>
-            {/* <h4>`"${props.title}"`</h4>
+            <h4>`"${props.title}"`</h4>
             <img src={props.image} alt={props.name} />
-            <p>{props.blurb}</p> */}
+            <p>{props.blurb}</p>
         </div>
     )
-}
+};
 
 export default ChampionCard
--- league-site/src/components/ChampionGrid.js
+++ league-site/src/components/ChampionGrid.js
@@ -13,13 +13,16 @@ const ChampionGrid = () => {
             .get("http://ddragon.leagueoflegends.com/cdn/9.21.1/data/en_US/champion.json")
             .then(response => {
                 console.log(response.data.data)
-                setChampion(response.data.data)
+                setChampion(response.data.data.Aatrox)
             })
     }, [])
 
+    console.log(champion)
+
     return (
         <div>
-            {Array.from(champion).map(champ => {
+            {Array.from(champion).map((champ) => {
+                
                 return (
                     <ChampionCard
                         key={champ.key}
@@ -32,6 +35,6 @@ const ChampionGrid = () => {
             })}
         </div>
     )
-}
+};
 
 export default ChampionGrid
